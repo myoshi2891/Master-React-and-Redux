9050f60a092c341885dba9f5cc23ca7b8f2118cf
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-09-19 22:39:11 +0900

    Removing Content

--- src/store/index.js
+++ src/store/index.js
@@ -7,7 +7,10 @@ const songsSlice = createSlice({
 		addSong(state, action) {
 			state.push(action.payload);
 		},
-		removeSong(state, action) {},
+		removeSong(state, action) {
+			const index = state.indexOf(action.payload);
+			state.splice(index, 1);
+		},
 	},
 });
 
@@ -18,4 +21,4 @@ const store = configureStore({
 });
 
 export { store };
-export const { addSong } = songsSlice.actions;
+export const { addSong, removeSong } = songsSlice.actions;
