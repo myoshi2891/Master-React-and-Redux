1bb5246d59e97ce7b63202794067b892f635a366
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-09-21 16:07:41 +0900

    Creating the Redux Store

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
-import './index.css';
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import App from "./App";
+import "./index.css";
+import { store } from "./store";
 
-const el = document.getElementById('root');
+const el = document.getElementById("root");
 const root = createRoot(el);
 
-root.render(<App />);
+root.render(
+	<Provider store={store}>
+		<App />
+	</Provider>
+);
