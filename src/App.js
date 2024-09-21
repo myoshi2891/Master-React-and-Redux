c245f2f196dc01d824c0bda51b9069ebc43d4039
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-09-21 15:08:19 +0900

    API Server Setup

--- src/App.js
+++ src/App.js
@@ -1,17 +1,5 @@
-import CarForm from './components/CarForm';
-import CarList from './components/CarList';
-import CarSearch from './components/CarSearch';
-import CarValue from './components/CarValue';
-
 function App() {
-  return (
-		<div className="container is-fluid">
-			<CarForm />
-			<CarSearch />
-			<CarList />
-			<CarValue />
-		</div>
-  );
+  return 'App!!!';
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,9 @@
-import "bulma/css/bulma.css";
-import React from "react";
-import { createRoot } from "react-dom/client";
-import { Provider } from "react-redux";
-import App from "./App";
-import { store } from "./store";
-import "./styles.css";
+import './index.css';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
 
-const el = document.getElementById("root");
+const el = document.getElementById('root');
 const root = createRoot(el);
 
-root.render(
-	<Provider store={store}>
-		<App />
-	</Provider>
-);
+root.render(<App />);
