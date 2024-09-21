f9d4e7a758a7dea538a4c39469467fcef9d10c36
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-09-21 17:28:33 +0900

    Wrapping up the Thunk

--- src/store/thunks/fetchUsers.js
+++ src/store/thunks/fetchUsers.js
@@ -2,7 +2,7 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 const fetchUsers = createAsyncThunk("users/fetch", async () => {
-	const response = await axios.get("http://localhost:3001/user");
+	const response = await axios.get("http://localhost:3001/users");
 
 	return response.data;
 });
