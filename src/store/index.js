7b8d7539d63e0adcc5e5d71e2165363b7460f58b
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-09-22 21:41:10 +0900

    Using the Generated Hook

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,21 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
+import { albumsApi } from "./apis/albumsApi";
 import { usersReducer } from "./slices/usersSlice";
 
 export const store = configureStore({
 	reducer: {
 		users: usersReducer,
+		[albumsApi.reducerPath]: albumsApi.reducer,
+	},
+	middleware: (getDefaultMiddleware) => {
+		return getDefaultMiddleware().concat(albumsApi.middleware);
 	},
 });
+
+setupListeners(store.dispatch);
 
 export * from "./thunks/fetchUsers";
 export * from "./thunks/addUser";
 export * from "./thunks/removeUser";
+export { useFetchAlbumsQuery } from "./apis/albumsApi";
