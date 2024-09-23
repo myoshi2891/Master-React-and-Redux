d3b1be9d44d7d3e4c25d830c1c863541ff7b6785
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-09-23 16:01:11 +0900

    Project Setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-	content: ["./src/**/*.{js,jsx,ts,tsx}"],
-	theme: {
-		extend: {
-			keyframes: {
-				shimmer: {
-					"100%": { transform: "translateX(100%)" },
-				},
-			},
-			animation: {
-				shimmer: "shimmer 1.5s infinite",
-			},
-		},
-	},
-	plugins: [],
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 };
