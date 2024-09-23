08f95abfee5298ea38cdbd00bd7173f3b8a5d063
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-09-23 14:50:34 +0900

    Creating the Photo

--- src/components/PhotosList.js
+++ src/components/PhotosList.js
@@ -1,11 +1,25 @@
-import { useFetchPhotosQuery } from "../store";
+import { useFetchPhotosQuery, useAddPhotoMutation } from "../store";
 import PhotosListItem from "./PhotosListItem";
+import Button from "./Button";
 
 function PhotosList({ album }) {
 	useFetchPhotosQuery(album);
+	const [addPhoto, addPhotoResults] = useAddPhotoMutation();
+	const handleAddPhoto = () => {
+		addPhoto(album);
+	};
+
 	return (
 		<div>
-			<PhotosListItem />
+			<div className="m-2 flex flex-row items-center justify-between">
+				<h3 className="text-lg font-bold">Photos In {album.title}</h3>
+				<Button
+					loading={addPhotoResults.isLoading}
+					onClick={handleAddPhoto}
+				>
+					+ Add Photo
+				</Button>
+			</div>
 		</div>
 	);
 }
