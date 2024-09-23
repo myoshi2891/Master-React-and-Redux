43de4b6c40432e0c1640684ca3a4cda2f18deecc
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-09-23 14:43:15 +0900

    Adding the Endpoints

--- src/components/PhotosList.js
+++ src/components/PhotosList.js
@@ -1,6 +1,8 @@
+import { useFetchPhotosQuery } from "../store";
 import PhotosListItem from "./PhotosListItem";
 
 function PhotosList({ album }) {
+	useFetchPhotosQuery(album);
 	return (
 		<div>
 			<PhotosListItem />
--- src/store/index.js
+++ src/store/index.js
@@ -2,14 +2,18 @@ import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
 import { albumsApi } from "./apis/albumsApi";
 import { usersReducer } from "./slices/usersSlice";
+import { photosApi } from "./apis/photosApi";
 
 export const store = configureStore({
 	reducer: {
 		users: usersReducer,
 		[albumsApi.reducerPath]: albumsApi.reducer,
+		[photosApi.reducerPath]: photosApi.reducer,
 	},
 	middleware: (getDefaultMiddleware) => {
-		return getDefaultMiddleware().concat(albumsApi.middleware);
+		return getDefaultMiddleware()
+			.concat(albumsApi.middleware)
+			.concat(photosApi.middleware);
 	},
 });
 
@@ -18,8 +22,16 @@ setupListeners(store.dispatch);
 export * from "./thunks/fetchUsers";
 export * from "./thunks/addUser";
 export * from "./thunks/removeUser";
+
 export {
 	useFetchAlbumsQuery,
 	useAddAlbumMutation,
 	useRemoveAlbumMutation,
 } from "./apis/albumsApi";
+
+export {
+	useFetchPhotosQuery,
+	useAddPhotoMutation,
+	useRemovePhotoMutation,
+} from "./apis/photosApi";
+
