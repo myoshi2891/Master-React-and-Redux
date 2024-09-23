047b3faf29206cb23f5af0cea873699480aa40ff
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-09-23 13:15:09 +0900

    Remove Implementation

--- src/store/apis/albumsApi.js
+++ src/store/apis/albumsApi.js
@@ -19,6 +19,14 @@ const albumsApi = createApi({
 	}),
 	endpoints(builder) {
 		return {
+			removeAlbum: builder.mutation({
+				query: (album) => {
+					return {
+						url: `/albums/${album.id}`,
+						method: "DELETE",
+					};
+				},
+			}),
 			addAlbum: builder.mutation({
 				invalidatesTags: (result, error, user) => {
 					return [{ type: "Album", id: user.id }];
@@ -52,5 +60,9 @@ const albumsApi = createApi({
 	},
 });
 
-export const { useFetchAlbumsQuery, useAddAlbumMutation } = albumsApi;
+export const {
+	useFetchAlbumsQuery,
+	useAddAlbumMutation,
+	useRemoveAlbumMutation,
+} = albumsApi;
 export { albumsApi };
