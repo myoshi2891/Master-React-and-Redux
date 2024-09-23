5981b2d3f4e7507664736d75c0bff833eb34f99c
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-09-23 16:30:40 +0900

    Typing of Props

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,5 +1,8 @@
 import type { Place } from "../api/Place";
+interface MapProps {
+	place: Place | null;
+}
 
-export default function Map() {
+export default function Map({ place }: MapProps) {
 	return <div>Map!!</div>;
 }
