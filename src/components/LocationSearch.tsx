f55c7af5379691652d84288b796987b6b8ad11eb
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-09-24 22:18:42 +0900

    Searching with the OSM API

--- src/components/LocationSearch.tsx
+++ src/components/LocationSearch.tsx
@@ -1,5 +1,5 @@
-import type { Place } from "../api/Place";
 import { useState } from "react";
+import { Place } from "../api/Place";
 
 interface LocationSearchProps {
 	onPlaceClick: (place: Place) => void;
@@ -7,15 +7,26 @@ interface LocationSearchProps {
 
 export default function LocationSearch({ onPlaceClick }: LocationSearchProps) {
 	const [places, setPlaces] = useState<Place[]>([]);
+	const [term, setTerm] = useState("");
 
-  return (
+	const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		console.log("need to find api", term);
+	};
+
+	return (
 		<div>
-			<form>
+			<form onSubmit={handleSubmit}>
 				<label className="font-bold" htmlFor="term">
 					Search
 				</label>
-				<input className="border border-gray-300 rounded-md shadow-sm focus:border-indigo-500 px-4 py-2 w-full" />
+				<input
+					className="border border-gray-300 rounded-md shadow-sm focus:border-indigo-500 px-4 py-2 w-full"
+					id="term"
+					value={term}
+					onChange={(e) => setTerm(e.target.value)}
+				/>
 			</form>
 		</div>
-  );
+	);
 }
