e6da2607cc11e725b289e547af7be40528f69e7e
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-09-26 22:04:42 +0900

    Rendering the List of Results

--- src/pages/search/SearchPage.tsx
+++ src/pages/search/SearchPage.tsx
@@ -1,10 +1,18 @@
-import type { SearchLoaderResult } from "./searchLoader";
 import { useLoaderData } from "react-router-dom";
+import PackageListItem from "../../components/PackageListItem";
+import type { SearchLoaderResult } from "./searchLoader";
 
 export default function SearchPage() {
-	const data = useLoaderData() as SearchLoaderResult;
+	const { searchResults } = useLoaderData() as SearchLoaderResult;
 
-	console.log(data.searchResults);
+	const renderedResults = searchResults.map((result) => {
+		return <PackageListItem pack={result} key={result.name} />;
+	});
 
-	return <div>SearchPage</div>;
+	return (
+		<div>
+			<h1 className="text-2xl font-bold my-6">SearchPage</h1>
+			<div className="space-y-4 mt-4">{renderedResults}</div>
+		</div>
+	);
 }
