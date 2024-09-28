3adcd65a6de97e08c50750325f4a75bff5bd2322
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-09-28 13:45:38 +0900

    Typing the Page and Loader Together

--- src/pages/details/DetailsPage.tsx
+++ src/pages/details/DetailsPage.tsx
@@ -1,3 +1,11 @@
+import { useLoaderData } from "react-router-dom";
+import { DetailsLoaderResult } from "./detailsLoader";
+
 export default function DetailsPage() {
-	return <div>DetailsPage</div>;
+	const { details } = useLoaderData() as DetailsLoaderResult;
+	return (
+		<div>
+			{details.name} - {details.description}
+		</div>
+	);
 }
