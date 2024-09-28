3919f0914cd226c4fac1d298957af4441239ba29
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-09-28 14:42:24 +0900

    Accessing the Data from a Loader

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,3 +1,9 @@
+import type { HomeLoaderResult } from "./homeLoader";
+import { useLoaderData } from "react-router-dom";
+
 export default function HomePage() {
+	const { featuredPackages } = useLoaderData() as HomeLoaderResult;
+	console.log(featuredPackages);
+
 	return <div>HomePage</div>;
 }
